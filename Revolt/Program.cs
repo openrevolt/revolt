7f67d0c240ff710e301e36628cabaf236739d76a
Andreas Venizelou <Bl4ckDn4@msn.com>
2024-10-14 17:42:18 +0300

    Add console renderer

--- Revolt/Program.cs
+++ Revolt/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Revolt;
+﻿global using System;
+global using System.Linq;
+
+namespace Revolt;
 
 internal class Program {
-    static void Main(string[] args) {
 
+    static void Main(string[] args) {
+        Console.Title = "Revolt";
+
+        Renderer.Start();
+
+        while (true) {
+            ConsoleKeyInfo key = Console.ReadKey();
+
+            switch (key.Key) {
+            case ConsoleKey.Escape : return;
+            case ConsoleKey.F5     : Renderer.Redraw(); break;
+
+            }
+        }
     }
 }
---
+++ Revolt/Renderer.cs
@@ -0,0 +1,53 @@
+﻿namespace Revolt;
+
+internal static class Renderer {
+    private static int width = 80;
+    private static int height = 20;
+
+    public static void Start() {
+        new Thread(SizeLoop) {
+            IsBackground = true
+        }.Start();
+    }
+
+    public static void SizeLoop() {
+        while (true) {
+            Thread.Sleep(200);
+
+            if (width == Console.WindowWidth && height == Console.WindowHeight) {
+                continue;
+            }
+
+            if (Console.WindowWidth <= 0 || Console.WindowHeight <= 0) {
+                continue;
+            }
+
+            if (OperatingSystem.IsWindows()) {
+                int w = Math.Min(Console.WindowWidth, Console.LargestWindowWidth);
+                int h = Math.Min(Console.WindowHeight, Console.LargestWindowHeight);
+                Console.BufferWidth = w;
+                Console.BufferHeight = h;
+            }
+
+            Redraw();
+        }
+    }
+
+    public static void Redraw() {
+        width = Console.WindowWidth;
+        height = Console.WindowHeight;
+
+        for (int y = 0; y < height; y++) {
+            if (y >= Console.WindowHeight) {
+                break;
+            }
+
+            Console.SetCursorPosition(0, y);
+
+            for (int x = 0; x < width; x++) {
+                Console.Write(x % 10);
+            }
+        }
+        
+    }
+}
