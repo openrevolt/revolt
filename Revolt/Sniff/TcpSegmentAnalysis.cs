baa4b12015b1e418ce3e010923c8cbd0c9c8b6ff
venizelou andreas <bl4ckdn4@msn.com>
2025-02-19 23:59:26 +0200

    AnalyzeWindow, CheckProtocolViolations, prep.

--- Revolt/Sniff/TcpSegmentAnalysis.cs
+++ Revolt/Sniff/TcpSegmentAnalysis.cs
@@ -6,8 +6,15 @@ using static Revolt.Sniff.Sniffer;
 namespace Revolt.Sniff;
 
 public sealed partial class Sniffer {
+    private const ushort FIN_MASK    = 0b00000000_00000001;
     private const ushort SYN_MASK    = 0b00000000_00000010;
+    private const ushort RES_MASK    = 0b00000000_00000100;
+    private const ushort PUS_MASK    = 0b00000000_00001000;
     private const ushort ACK_MASK    = 0b00000000_00010000;
+    private const ushort URG_MASK    = 0b00000000_00100000;
+    private const ushort ECH_MASK    = 0b00000000_01000000;
+    private const ushort WIN_MASK    = 0b00000000_10000000;
+    private const ushort ACC_MASK    = 0b00000001_00000000;
     private const ushort SYNACK_MASK = 0b00000000_00010010;
 
     private readonly ConcurrentDictionary<FourTuple, (HashSet<uint>, HashSet<uint>)> sequenceTrackers = new ConcurrentDictionary<FourTuple, (HashSet<uint>, HashSet<uint>)>();
@@ -72,39 +79,59 @@ public sealed partial class Sniffer {
         return timestampAck - timestampSyn;
     }
 
-    private void AnalyzeSequenceNo(in Segment lastSegment, ConcurrentQueue<Segment> stream, StreamCount count) {
-        if (!sequenceTrackers.TryGetValue(lastSegment.fourTuple, out (HashSet<uint>, HashSet<uint>) sequenceTracker)) {
+    private void AnalyzeSequenceNo(in Segment segment, ConcurrentQueue<Segment> stream, StreamCount count) {
+        if (!sequenceTrackers.TryGetValue(segment.fourTuple, out (HashSet<uint>, HashSet<uint>) sequenceTracker)) {
             sequenceTracker = (new HashSet<uint>(), new HashSet<uint>());
-            sequenceTrackers[lastSegment.fourTuple] = sequenceTracker;
+            sequenceTrackers[segment.fourTuple] = sequenceTracker;
         }
 
-        //bool isPhantomByte = false;
-
-        int sourceHash = unchecked(lastSegment.fourTuple.sourceIP.GetHashCode() + lastSegment.fourTuple.sourcePort);
-        int destinationHash = unchecked(lastSegment.fourTuple.destinationIP.GetHashCode() + lastSegment.fourTuple.destinationPort);
+        int sourceHash = unchecked(segment.fourTuple.sourceIP.GetHashCode() + segment.fourTuple.sourcePort);
+        int destinationHash = unchecked(segment.fourTuple.destinationIP.GetHashCode() + segment.fourTuple.destinationPort);
 
         HashSet<uint> tracker = sourceHash < destinationHash ? sequenceTracker.Item1 : sequenceTracker.Item2;
 
-        if (tracker.Contains(lastSegment.sequenceNo)) {
+        bool isSyn = (segment.flags & SYN_MASK) == SYN_MASK;
+        bool isFin = (segment.flags & FIN_MASK) == FIN_MASK;
+        bool hasPhantomByte = isSyn || isFin;
+
+        if (tracker.Contains(segment.sequenceNo)) {
             Interlocked.Increment(ref count.retransmission);
         }
         else {
-            tracker.Add(lastSegment.sequenceNo);
+            tracker.Add(segment.sequenceNo);
         }
+
+        uint size = segment.size;
+        if (hasPhantomByte && size == 0) size = 1;
 
         if (sourceHash < destinationHash) {
-            if (lastSegment.sequenceNo > count.nextSeqNoA) {
+            if (segment.sequenceNo > count.nextSeqNoA) {
                 Interlocked.Increment(ref count.loss);
             }
-            count.nextSeqNoA = unchecked(lastSegment.sequenceNo + lastSegment.size);
+            count.nextSeqNoA = unchecked(segment.sequenceNo + size);
         }
         else {
-            if (lastSegment.sequenceNo > count.nextSeqNoB) {
+            if (segment.sequenceNo > count.nextSeqNoB) {
                 Interlocked.Increment(ref count.loss);
             }
-            count.nextSeqNoB = unchecked(lastSegment.sequenceNo + lastSegment.size);
+            count.nextSeqNoB = unchecked(segment.sequenceNo + size);
         }
-
     }
+
+    private void AnalyzeWindow(in Segment segment, StreamCount count) {
+        if (segment.window == 0) {
+            //zero window size
+        }
+    }
+
+    private void CheckProtocolViolations(in Segment segment, StreamCount count) {
+        bool isSyn = (segment.flags & SYN_MASK) == SYN_MASK;
+        bool isFin = (segment.flags & FIN_MASK) == FIN_MASK;
+
+        if ((isSyn || isFin) && segment.size > 0) {
+            //protocol violation
+        }
+    }
+
 
 }
